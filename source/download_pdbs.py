3216f483f3fccf0e03c2bf5110435b1e3fbe5ed5
Samo <samofanci@gmail.com>
2025-11-05 14:06:05 +0100

    security update

--- source/download_pdbs.py
+++ source/download_pdbs.py
@@ -5,11 +5,15 @@ import os
 
 def load_prots(prots_path):
     return pd.read_csv(prots_path, sep='\t')['uniprot_id']
+
+def load_key(key_path):
+    with open(key_path, 'r') as f:
+        return f.read()
     
-def download_pdbs(ids):
+def download_pdbs(ids, key):
     for prot_id in ids:
         URL = f"https://alphafold.ebi.ac.uk/api/prediction/{prot_id}?"
-        params = {'key' : "AIzaSyCeurAJz7ZGjPQUtEaerUkBZ3TaBkXrY94"}
+        params = {'key' : key}
         if not os.path.exists('../data/pdbs'):
             os.mkdir('../data/pdbs/')
 
@@ -29,6 +33,6 @@ def download_pdbs(ids):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--prots_path', default='../data/prots.csv', help='Path to a protein tsv-like file. Should contain a column name "uniprot_id", and the values should be Uniprot IDs of the proteins to be downloaded from AFDB.')
-
+    parser.add_argument('--key_path', default='../data/key.txt', help='Location of the security key file for AFDB. Text file should contain ONLY the key.')
     args = parser.parse_args()
-    download_pdbs(load_prots(args.prots_path))
+    download_pdbs(load_prots(args.prots_path), load_key(args.key_path))
