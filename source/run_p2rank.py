df259f6f7b46f48cd36ceeed3ae411805f2dfa6c
Samo <samofanci@gmail.com>
2025-11-05 16:29:42 +0100

    prepared p2rank predictions

---
+++ source/run_p2rank.py
@@ -0,0 +1,24 @@
+import os
+import argparse
+import glob
+
+def create_ds_file(pdbs_path):
+    paths = glob.glob(f'{pdbs_path}/*.pdb')
+    with open('../data/pdbs.ds', 'w') as f:
+        f.write('\n'.join(paths))
+
+def run_p2rank(p2rank_path, ds_path, outputd_dir):
+    if not os.path.exists(outputd_dir):
+        os.mkdir(outputd_dir)
+
+    os.system(f'{p2rank_path}\\prank predict {ds_path} -o {outputd_dir}')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--p2rank_path', default='..\\p2rank', help='p2rank root directory')
+    parser.add_argument('--output_path', default='../data/p2rank_output', help='Output directory path')
+    parser.add_argument('--pdbs_path', default='../data/pdbs', help='Path to the directory where .pdb files are stored')
+
+    args = parser.parse_args()
+    create_ds_file(args.pdbs_path)
+    run_p2rank(args.p2rank_path, args.pdbs_path, args.output_path)
