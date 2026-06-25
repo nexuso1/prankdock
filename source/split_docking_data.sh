f50e4ec9ca1d75d158c2e806a88e2e34e04c436e
Sam <samofanci@gmail.com>
2026-06-25 12:33:31 +0200

    bugfix

--- source/split_docking_data.sh
+++ source/split_docking_data.sh
@@ -4,41 +4,42 @@ root_dir=$1
 splits_dir=$2
 n_splits=$3
 
-count=0
+prot_count=0
 declare qts=()
 declare boxes=()
 declare configs=()
 for file in $(ls "$root_dir"/**/*.pdbqt); do
-        qts+=($file)
-        boxes+=(${file%.pdbqt}.box.pdb)
-        configs+=(${file%.pdbqt}.box.txt)
-	count=$((count+1))
+	qts+=($file)
+	boxes+=(${file%.pdbqt}.box.pdb)
+	configs+=(${file%.pdbqt}.box.txt)
+	((prot_count++))
 done
 
-echo $count
+echo $prot_count
 echo ${qts[0]}
 echo ${boxes[0]}
 echo ${configs[0]}
 
-size=$((count / $n_splits))
-
+split_size=$(($prot_count / $n_splits))
+current_split_size=0
+split_idx=0
 mkdir "$splits_dir"
-for i in $(seq 0 $(($n_splits - 1))); do
-	echo $i
-    out_dir=./"$splits_dir"/splits_"$i"
-	mkdir "$out_dir"
-	for j in $(seq 0 $size); do
-		index=$((i * size + j))
-		cp "${qts[index]}" "$out_dir"
-		cp "${boxes[index]}" "$out_dir"
-		cp "${configs[index]}" "$out_dir"
-	done
-done
 
-for i in $(seq $(($count - $count % $size)) $(($count - 1))); do
+out_dir="$splits_dir"/splits_"$split_idx"
+mkdir "$out_dir"
+
+
+for i in $(seq 0 $(($prot_count - 1))); do
 	echo $i
-	s_idx=$((n_splits - 1))
-	cp "${qts[i]}" "./splits_$s_idx/"
-	cp "${boxes[i]}" "./splits_$s_idx/"
-	cp "${configs[i]}" "./splits_$s_idx/"
+    
+	cp "${qts[i]}" "$out_dir"
+	cp "${boxes[i]}" "$out_dir"
+	cp "${configs[i]}" "$out_dir"
+	((current_split_size++))
+	if [ "$current_split_size" -eq "$split_size" ]; then
+		((split_idx++))
+		current_split_size=0
+		out_dir="$splits_dir"/splits_"$split_idx"
+		mkdir "$out_dir"
+	fi
 done
